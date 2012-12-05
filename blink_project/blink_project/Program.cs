5b5dc56f02747368d2312a023f6939435ed9803e
punker76 <punkerat76@jkarger.de>
2012-12-05 18:09:45 +0100

    add simple blink project

---
+++ blink_project/blink_project/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading;
+using Microsoft.SPOT.Hardware;
+using SecretLabs.NETMF.Hardware.Netduino;
+
+namespace blink_project
+{
+  public class Program
+  {
+    public static void Main() {
+      // write your code here
+      var led = new OutputPort(Pins.ONBOARD_LED, false);
+
+      while (true) {
+        led.Write(true); // turn on the led
+        Thread.Sleep(250); // sleep for 250 ms
+        led.Write(false); // turn off the led
+        Thread.Sleep(250); // sleep for 250 ms
+      }
+    }
+  }
+}
